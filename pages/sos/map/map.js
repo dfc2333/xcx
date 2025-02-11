e3ca7e214815e710d56e942caf3e367b5315b910
dfc2333 <dfc2333@outlook.com>
2025-02-11 19:36:18 +0800

    2.11.5
    符合大小；地图更新

--- pages/sos/map/map.js
+++ pages/sos/map/map.js
@@ -1,66 +1,48 @@
 // pages/sos/map/map.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    latitude: 23.10287,
+    longitude: 113.26795,
+    markers: [{
+      id: 1,
+      latitude: 23.10287,
+      longitude: 113.26795,
+      name: 'gzwz'
+    }],
+    covers: [{
+      latitude: 23.10287,
+      longitude: 113.26795,
+      iconPath: '/image/location.png'
+    }],
+    lg:'',
+    la:''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude:23.09401,
+        longitude:113.29119,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
