e3a9144391efdf8743b23dba9a4b17ee4204140a
dfc2333 <dfc2333@outlook.com>
2025-02-11 13:25:45 +0800

    2.11.2
    相似三角形

--- pages/tools/cliff/cliff.js
+++ pages/tools/cliff/cliff.js
@@ -1,22 +1,52 @@
 // index.js
 Page({
   data: {
-    res:'这里将会显示你的悬崖高度',
+    res:'这里将显示悬崖高度近似值',
+    hand:'',
+    dis:'',
+    rul:''
   },
-  calc: function(e){
-    var bt = e.detail.value
-    if (parseFloat(bt)>0 & parseFloat(bt)<200){
-    var h = (100-bt)/0.003
+  hnd: function(e){
+    this.setData({
+      hand:e.detail.value
+    })
+    console.log('hndchanged',this.data.hand)
+  },
+  ds: function(e){
+    this.setData({
+      dis:e.detail.value
+    })
+    console.log('dschanged',this.data.dis)
+  },
+  rl: function(e){
+    this.setData({
+      rul:e.detail.value
+    })
+    console.log('rlchanged',this.data.rul)
+  },
+  calc: function(){
+    var hand = this.data.hand
+    var dis = this.data.dis
+    var rul = this.data.rul
+    if (parseFloat(hand) && parseFloat(dis) && parseFloat(rul)){
+    var h = rul/100*dis/hand
     this.setData({
       res:Math.round(h)
-    })}else if(bt===''){
+    })}else if(hand===''||dis===''||rul===''){
       this.setData({
-        res:"这里将显示悬崖高度近似值"
+        res:"没填完呢"
       })
     }else{
       this.setData({
         res:'神金'
       })
     }
+  },
+  del: function(){
+    this.setData({
+      hand: '',
+      dis:'',
+      rul:''
+    })
   }
 })
--- pages/tools/water/water.js
+++ pages/tools/water/water.js
@@ -4,9 +4,9 @@ Page({
     res: '这里将显示你的海拔高度近似值'
   },
   calc: function(e){
-    var bt = e.detail.value
+    var bt = e.detail.value;
     if (parseFloat(bt)>0 & parseFloat(bt)<200){
-    var h = (100-bt)/0.003
+    var h = (100-bt)/0.003;
     this.setData({
       res:Math.round(h)
     })}else if(bt===''){
