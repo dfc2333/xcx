e440092305cc188920669bbf2c433813f9bf4370
sti <86592209+sti-233@users.noreply.github.com>
2025-02-16 12:38:48 +0800

    chore: nth

--- pages/tools/cliff/cliff.js
+++ pages/tools/cliff/cliff.js
@@ -1,52 +1,78 @@
 // index.js
 Page({
   data: {
-    res:'这里将显示悬崖高度近似值',
-    hand:'',
-    dis:'',
-    rul:''
+    res: "...",
+    hand: "",
+    dis: "",
+    rul: "",
   },
-  hnd: function(e){
+  onHandInput(e) {
     this.setData({
-      hand:e.detail.value
-    })
-    console.log('hndchanged',this.data.hand)
+      hand: e.detail.value,
+      empty: false,
+    });
+    console.log("high changed", this.data.high);
   },
-  ds: function(e){
+  onDisInput(e) {
     this.setData({
-      dis:e.detail.value
-    })
-    console.log('dschanged',this.data.dis)
+      dis: e.detail.value,
+      empty: false,
+    });
+    console.log("high changed", this.data.high);
   },
-  rl: function(e){
+  onRulInput(e) {
     this.setData({
-      rul:e.detail.value
-    })
-    console.log('rlchanged',this.data.rul)
+      rul: e.detail.value,
+      empty: false,
+    });
+    console.log("high changed", this.data.high);
   },
-  calc: function(){
-    var hand = this.data.hand
-    var dis = this.data.dis
-    var rul = this.data.rul
-    if (parseFloat(hand) && parseFloat(dis) && parseFloat(rul)){
-    var h = rul/100*dis/hand
+/*
+  hnd: function (e) {
     this.setData({
-      res:Math.round(h)
-    })}else if(hand===''||dis===''||rul===''){
+      hand: e.detail.value,
+    });
+    console.log("hndchanged", this.data.hand);
+  },
+  ds: function (e) {
+    this.setData({
+      dis: e.detail.value,
+    });
+    console.log("dschanged", this.data.dis);
+  },
+  rl: function (e) {
+    this.setData({
+      rul: e.detail.value,
+    });
+    console.log("rlchanged", this.data.rul);
+  },
+  */
+  calc: function () {
+    var hand = this.data.hand;
+    var dis = this.data.dis;
+    var rul = this.data.rul;
+    if (parseFloat(hand) && parseFloat(dis) && parseFloat(rul)) {
+      var h = ((rul / 100) * dis) / hand;
+      console.log("[Debug] h ="+h)
       this.setData({
-        res:"没填完呢"
-      })
-    }else{
+        res: Math.round(h),
+      });
+    } else if (hand === "" || dis === "" || rul === "") {
       this.setData({
-        res:'神金'
-      })
+        res: "没填完呢",
+      });
+    } else {
+      this.setData({
+        res: "神金",
+      });
     }
   },
-  del: function(){
+  del: function () {
     this.setData({
-      hand: '',
-      dis:'',
-      rul:''
-    })
-  }
-})
+      hand: "",
+      dis: "",
+      rul: "",
+      res: "..."
+    });
+  },
+});
